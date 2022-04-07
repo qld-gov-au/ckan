80eb5ebb6a73271b6463eefec168ad022d97ebab
ThrawnCA <shell_layer-github@yahoo.com.au>
2022-04-07 11:47:56 +1000

    [QOL-8576] fix isoformat syntax so lint is happy
    - raw string literals aren't allowed, but Python 2 'isoformat' can't handle unicode,
    so use the 'str' function, which is documented to have the same effect

--- ckan/cli/user.py
+++ ckan/cli/user.py
@@ -242,7 +242,7 @@ def list_tokens(username):
                 Strip out microseconds to force formatting as isoformat doesnt
                 have a timespec param on Python 2.
                 """
-                accessed = datetime(
+                accessed = str(datetime(
                     accessed.year,
                     accessed.month,
                     accessed.day,
@@ -250,7 +250,7 @@ def list_tokens(username):
                     accessed.minute,
                     accessed.second).isoformat(b" ")
             else:
-                accessed = accessed.isoformat(" ", u"seconds")
+                accessed = accessed.isoformat(u" ", u"seconds")
 
         else:
             accessed = u"Never"

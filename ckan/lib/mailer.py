dc6c1a95553235e53710451e0a4fa07a4d118caf
antuarc <carl.antuar@smartservice.qld.gov.au>
2022-04-22 15:56:12 +1000

    [QOL-8515] optionally hide CKAN version in site status and emails
    - copied from 2.8.8 QGOV branch

--- ckan/lib/mailer.py
+++ ckan/lib/mailer.py
@@ -58,6 +58,8 @@ def _mail_recipient(recipient_name, recipient_email,
     msg['From'] = _("%s <%s>") % (sender_name, mail_from)
     msg['To'] = u"%s <%s>" % (recipient_name, recipient_email)
     msg['Date'] = utils.formatdate(time())
+    if not ckan.common.asbool(config.get('ckan.hide_version')):
+        msg['X-Mailer'] = "CKAN %s" % ckan.__version__
     msg['X-Mailer'] = "CKAN %s" % ckan.__version__
     # Check if extension is setting reply-to via headers or use config option
     if reply_to and reply_to != '' and not msg['Reply-to']:

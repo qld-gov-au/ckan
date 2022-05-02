60ead035cd6eca7195db072d98a927354891d851
ThrawnCA <shell_layer-github@yahoo.com.au>
2022-05-03 09:09:30 +1000

    [QOL-8785] fix missing imports and make Flake8 happy

--- ckan/lib/uploader.py
+++ ckan/lib/uploader.py
@@ -7,6 +7,7 @@ import datetime
 import logging
 import magic
 import mimetypes
+import six
 from six.moves.urllib.parse import urlparse
 
 from werkzeug.datastructures import FileStorage as FlaskFileStorage
@@ -14,7 +15,7 @@ from werkzeug.datastructures import FileStorage as FlaskFileStorage
 import ckan.lib.munge as munge
 import ckan.logic as logic
 import ckan.plugins as plugins
-from ckan.common import config, aslist
+from ckan.common import config, aslist, request, response
 
 ALLOWED_UPLOAD_TYPES = (cgi.FieldStorage, FlaskFileStorage)
 MB = 1 << 20
@@ -282,6 +283,7 @@ class Upload(object):
             return self._flask_download(id, filepath)
 
     def _pylons_download(self, filepath):
+        import paste
         fileapp = paste.fileapp.FileApp(filepath)
 
         status, headers, app_iter = request.call_application(fileapp)
@@ -361,7 +363,7 @@ class ResourceUpload(object):
                     self.mimetype = magic.from_buffer(self.upload_file.read(),
                                                       mime=True)
                     self.upload_file.seek(0, os.SEEK_SET)
-                except IOError as e:
+                except IOError:
                     # Not that important if call above fails
                     self.mimetype = None
 
@@ -427,7 +429,7 @@ class ResourceUpload(object):
         if self.clear:
             try:
                 os.remove(filepath)
-            except OSError as e:
+            except OSError:
                 pass
 
     def delete(self, id, filename=None):
@@ -446,6 +448,7 @@ class ResourceUpload(object):
             return self._flask_download(id, filepath)
 
     def _pylons_download(self, filepath):
+        import paste
         fileapp = paste.fileapp.FileApp(filepath)
         # may throw OSError, which should be handled by the controller
         # which will wrap it with abort(404, _('Resource data not found'))

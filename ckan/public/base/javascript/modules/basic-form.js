7fd35972f52ab0f8bd3fb9fab37a82f489787df9
Ross Webster <ross.webster@smartservice.qld.gov.au>
2023-11-23 16:23:57 +1000

    [QOLDEV-667] Change url input type on form submission

--- ckan/public/base/javascript/modules/basic-form.js
+++ ckan/public/base/javascript/modules/basic-form.js
@@ -1,4 +1,5 @@
 this.ckan.module('basic-form', function ($) {
+  var urlField = $('#field-resource-url');
   return {
     initialize: function () {
       var message = this._('There are unsaved modifications to this form');
@@ -12,6 +13,13 @@ this.ckan.module('basic-form', function ($) {
       this.el.on('submit', this._onSubmit);
     },
     _onSubmit: function () {
+
+      // Change input type to text if Upload is selected
+      if ($('#resource-url-upload').prop('checked')) {
+        urlField.attr('type', 'text');
+      } else {
+        urlField.attr('type', 'url');
+      }
 
       // The button is not disabled immediately so that its value can be sent
       // the first time the form is submitted, because the "save" field is

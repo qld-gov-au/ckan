ea624eb71af32e0fb8dc7e46ab09647f91723832
ThrawnCA <shell_layer-github@yahoo.com.au>
2024-01-16 16:53:11 +1000

    [QOLDEV-692] clean lint to make pyright happier

--- ckan/lib/search/index.py
+++ ckan/lib/search/index.py
@@ -248,9 +248,9 @@ class PackageSearchIndex(SearchIndex):
                 except DateParserError:
                     log.warning('%r: %r value of %r is not a valid date', pkg_dict['id'], key, value)
                     continue
-            if type(value) is dict:
+            if isinstance(value, dict):
                 # Solr 8+ chokes on passing dict values unless doing an atomic update
-                value = str(value)
+                value = json.dumps(value)
             new_dict[key] = value
 
         pkg_dict = new_dict

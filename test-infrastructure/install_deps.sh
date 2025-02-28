9c8c00de8f6445115a939d7ec6323ccfd09a0321
amercader <amercadero@gmail.com>
2025-02-28 12:24:59 +0100

    Bump setuptools to avoid pysolr build errors with setuptools-scm
    Error:
    
    https://gist.github.com/amercader/b047186b9ab6cab0e8e47cd87b1aeebe
    
    Collecting pysolr==3.9.0 (from -r requirements.txt (line 102))
      Downloading pysolr-3.9.0.tar.gz (55 kB)
      Preparing metadata (setup.py) ... error
      error: subprocess-exited-with-error
    
      × python setup.py egg_info did not run successfully.
      │ exit code: 1
      ╰─> [68 lines of output]
          /tmp/pip-install-iyrzysc_/pysolr_1ac89b9533694450b0e9e43d23a237e7/.eggs/setuptools_scm-8.2.0-py3.9.egg/setuptools_scm/_integration/setuptools.py:31: RuntimeWarning:
          ERROR: setuptools==58.1.0 is used in combination with setuptools-scm>=8.x
    
          Your build configuration is incomplete and previously worked by accident!
          setuptools-scm requires setuptools>=61
    
          Suggested workaround if applicable:
           - migrating from the deprecated setup_requires mechanism to pep517/518
             and using a pyproject.toml to declare build dependencies
             which are reliably pre-installed before running the build tools
    
    Refs:
    
    https://github.com/pypa/setuptools-scm/issues/1112
    https://github.com/pypa/setuptools-scm/issues/1111

--- test-infrastructure/install_deps.sh
+++ test-infrastructure/install_deps.sh
@@ -7,6 +7,7 @@ apt install -y postgresql-client
 
 #Python Dependencies
 pip install -U pip
+pip install -U setuptools
 pip install -r requirements.txt
 pip install -r dev-requirements.txt
 pip install -e .

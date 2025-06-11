a6c6de62a9b630b685da8d0f33613d825bd5fa32
antuarc <carl.antuar@smartservice.qld.gov.au>
2025-06-11 12:42:47 +1000

    [QOLSVC-10616] drop redundant transaction declarations in _drop_indexes
    - A transaction is already active and nesting is discouraged

--- ckanext/datastore/backend/postgres.py
+++ ckanext/datastore/backend/postgres.py
@@ -730,18 +730,16 @@ def _drop_indexes(context: Context, data_dict: dict[str, Any],
             AND idx.indisprimary = false
             AND t.relname = :relname
         """.format(unique='true' if unique else 'false')
-    with context['connection'].begin():
-        indexes_to_drop = context['connection'].execute(
-            sa.text(sql_get_index_string),
-            {"relname": data_dict['resource_id']}
-        ).fetchall()
+    indexes_to_drop = context['connection'].execute(
+        sa.text(sql_get_index_string),
+        {"relname": data_dict['resource_id']}
+    ).fetchall()
 
-    with context['connection'].begin():
-        context['connection'].execute(_LOCK_TIMEOUT_SQL)
-        for index in indexes_to_drop:
-            context['connection'].execute(sa.text(
-                sql_drop_index.format(sa.column(index[0]))
-            ))
+    context['connection'].execute(_LOCK_TIMEOUT_SQL)
+    for index in indexes_to_drop:
+        context['connection'].execute(sa.text(
+            sql_drop_index.format(sa.column(index[0]))
+        ))
 
 
 def _get_index_names(connection: Any, resource_id: str):
@@ -902,6 +900,9 @@ Indexes: TypeAlias = "Optional[list[Union[str, list[str]]]]"
 
 
 def create_indexes(context: Context, data_dict: dict[str, Any]):
+    """ The connection object at context['connection']
+    should already have an active transaction before calling this.
+    """
     connection = context['connection']
 
     indexes: Indexes = cast(Indexes, datastore_helpers.get_list(

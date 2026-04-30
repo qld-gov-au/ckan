cd4c95a36427717c71e395d3336417fc30a8522a
ThrawnCA <carl.antuar@smartservice.qld.gov.au>
2026-04-30 10:07:09 +1000

    [QOLDEV-1884] Shorten the lock timeout on dropping datastore tables to avoid deadlocks

--- ckanext/datastore/backend/postgres.py
+++ ckanext/datastore/backend/postgres.py
@@ -59,6 +59,8 @@ _engines: Dict[str, Engine] = {}
 WhereClauses: TypeAlias = "list[tuple[str, dict[str, Any]] | tuple[str]]"
 
 _TIMEOUT = 60000  # milliseconds
+_LOCK_TIMEOUT = 20000
+_LOCK_TIMEOUT_SQL = sa.text(u"SET lock_timeout = {}".format(_LOCK_TIMEOUT))
 
 # See http://www.postgresql.org/docs/9.2/static/errcodes-appendix.html
 _PG_ERR_CODE = {
@@ -742,6 +744,8 @@ def _drop_indexes(context: Context, data_dict: dict[str, Any],
         sa.text(sql_get_index_string),
         {"relname": data_dict['resource_id']}
     ).fetchall()
+
+    context['connection'].execute(_LOCK_TIMEOUT_SQL)
     for index in indexes_to_drop:
         context['connection'].execute(sa.text(
             sql_drop_index.format(sa.column(index[0]))
@@ -906,6 +910,9 @@ Indexes: TypeAlias = "Optional[list[Union[str, list[str]]]]"
 
 
 def create_indexes(context: Context, data_dict: dict[str, Any]):
+    """ The connection object at context['connection']
+    should already have an active transaction before calling this.
+    """
     connection = context['connection']
 
     indexes: Indexes = cast(Indexes, datastore_helpers.get_list(
@@ -1715,7 +1722,7 @@ def _create_fulltext_trigger(connection: Any, resource_id: str):
 def upsert(context: Context, data_dict: dict[str, Any]):
     '''
     This method combines upsert insert and update on the datastore. The method
-    that will be used is defined in the mehtod variable.
+    that will be used is defined in the method variable.
 
     Any error results in total failure! For now pass back the actual error.
     Should be transactional.
@@ -2111,6 +2118,7 @@ class DatastorePostgresqlBackend(DatastoreBackend):
 
         with engine.begin() as conn:
             context["connection"] = conn
+            conn.execute(_LOCK_TIMEOUT_SQL)
             # check if table exists
             if 'filters' not in data_dict:
                 conn.execute(sa.text('DROP TABLE {0} CASCADE'.format(
@@ -2155,6 +2163,7 @@ class DatastorePostgresqlBackend(DatastoreBackend):
             context['connection'].execute(sa.text(
                 f"SET LOCAL statement_timeout TO {timeout}"
             ))
+            context['connection'].execute(_LOCK_TIMEOUT_SQL)
             result = context['connection'].execute(sa.text(
                 'SELECT * FROM pg_tables WHERE tablename = :table'
             ), {"table": data_dict['resource_id']}).fetchone()
@@ -2410,6 +2419,7 @@ class DatastorePostgresqlBackend(DatastoreBackend):
         sql = f'ANALYZE {identifier(resource_id)}'
         with get_write_engine().connect() as conn:
             try:
+                conn.execute(_LOCK_TIMEOUT_SQL)
                 conn.execute(sa.text(sql))
             except DatabaseError as err:
                 raise DatastoreException(err)
@@ -2457,6 +2467,7 @@ def drop_function(name: str, if_exists: bool):
 
 def _write_engine_execute(sql: str):
     with get_write_engine().begin() as conn:
+        conn.execute(_LOCK_TIMEOUT_SQL)
         conn.execute(sa.text(sql))
 
 
